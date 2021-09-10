ad332f2270dcb397da21997113418ae89254b4b1
VernitaJ <gusgouve@student.gu.se>
2021-09-10 14:26:19 +0200

    Fix server start script

--- server/controllers/companies.js
+++ server/controllers/companies.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var Company = ('../models/company');
 
 // Companies - database functions
-app.get('/companies', function(req, res, next) {
+router.get('/companies', function(req, res, next) {
     Company.find(function(err, companies) {
         if (err) {return next(err); } 
         res.json({"companies": companies});
     });
 });
 
-app.post('/companies', function(req, res, next) {
+router.post('/companies', function(req, res, next) {
     var company = new Company(req.body);
     company.save(function(err) {
         if (err) { return next(err); }
