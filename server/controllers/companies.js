0b03eab98df594098c729e8b0c73fb3d5d473e59
VernitaJ <gusgouve@student.gu.se>
2021-09-16 13:11:06 +0200

    update to schema and controllers

--- server/controllers/companies.js
+++ server/controllers/companies.js
@@ -1,21 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Company = ('../models/company');
+var Company = require("../models/company");
 
 // Companies - database functions
-router.get('/companies', function(req, res, next) {
-    Company.find(function(err, companies) {
-        if (err) {return next(err); } 
-        res.json({"companies": companies});
-    });
+router.get("/companies", function (req, res, next) {
+  Company.find(function (err, companies) {
+    if (err) {
+      return next(err);
+    }
+    res.json({ companies: companies });
+  });
 });
 
-router.post('/companies', function(req, res, next) {
-    var company = new Company(req.body);
-    company.save(function(err) {
-        if (err) { return next(err); }
-        res.status(201).json(company);
-    });
+router.get("/companies/:id", function (req, res, next) {
+  Company.findById(req.params.id, (err, company) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(company);
+  });
+});
+
+router.post("/companies", function (req, res, next) {
+  var company = new Company(req.body);
+  company.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.status(201).json(company);
+  });
+});
+
+// router.put("/companies/:id", function (req, res) {
+//   var id = req.params.id;
+//   var updated_company = {
+//     name: req.body.name,
+//     adress: req.body.adress,
+//     contact: req.body.contact,
+//     category: req.body.category,
+//     deals: ["613fb14024606a0514aeab09", "613fb18424606a0514aeab0d"],
+//   };
+//   Company[id] = updated_company;
+//   res.json(updated_company);
+// });
+
+router.put("/companies/:id", (req, res) => {
+  var id = req.params.id;
+  Company.findById(id, function (err, company) {
+    if (err) {
+      return next(err);
+    }
+    if (company == null) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+    company.name = req.body.name;
+    company.deals.push(req.body.deals); //adds to the array - only one deal at a time
+    company.save();
+    res.json(company);
+  });
 });
 
 module.exports = router;
