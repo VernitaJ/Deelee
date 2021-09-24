ab3caf938e5ed911e20f5a7502f43786a158ea13
VernitaJ <gusgouve@student.gu.se>
2021-09-24 11:20:44 +0200

    Changed client to include map

---
+++ client/src/views/User.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-jumbotron header="Users" lead="All the users">
+      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
+      <p>Message from the server:<br/>
+      {{ message }}</p>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  methods: {
+    getMessage() {
+      Api.get('/')
+        .then(response => {
+          this.message = response.data.message
+        })
+        .catch(error => {
+          this.message = error
+        })
+    }
+  }
+}
+</script>
+
+<style>
+.btn_message {
+  margin-bottom: 1em;
+}
+</style>
