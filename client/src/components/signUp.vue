04047274b3ecae865746d622bfc083ac59c2c9bc
VernitaJ <gusgouve@student.gu.se>
2021-10-14 11:39:21 +0200

    mongodb adjustments

--- client/src/components/signUp.vue
+++ client/src/components/signUp.vue
@@ -1,34 +1,62 @@
 <template>
-<div>
-
-<form @submit.prevent="handleSubmit">
   <div>
-  <h1>Sign Up</h1>
-</div>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <h1>Sign Up</h1>
+      </div>
 
-  <div class ="form-group">
-    <input type="text" class="form-control" v-model="firstName" placeholder="First Name"/>
-  </div>
-<div class ="form-group">
-    <input type="text" class="form-control" v-model="lastName" placeholder="Last Name"/>
-  </div>
-<div class ="form-group">
-    <input type="number" class="form-control" v-model="age" placeholder="Age"/>
-  </div>
-<div class ="form-group">
-    <input type="text" class="form-control" v-model="location" placeholder="Location"/>
-  </div>
-<div class ="form-group">
-    <input type="email" class="form-control" v-model="email" placeholder="Email"/>
-  </div>
-<div class ="form-group">
-    <input type="password" class="form-control" v-model="password" placeholder="Password"/>
-  </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          v-model="firstName"
+          placeholder="First Name"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          v-model="lastName"
+          placeholder="Last Name"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="number"
+          class="form-control"
+          v-model="age"
+          placeholder="Age"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          v-model="location"
+          placeholder="Location"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="email"
+          class="form-control"
+          v-model="email"
+          placeholder="Email"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          v-model="password"
+          placeholder="Password"
+        />
+      </div>
 
- <button class="btn btn-primary btn-block">Submit</button>
-</form>
-</div>
-
+      <button class="btn btn-primary btn-block">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -56,16 +84,18 @@ export default {
         email: this.email,
         password: this.password
       }
-      Api.post('/users', newUser)
-        .then(res => {
+      Api.post('/users', newUser).then(
+        (res) => {
           console.log(res)
           this.$bvModal.msgBoxOk('SignUp Successful', this.$router.go(0))
-        }, err => {
+        },
+        (err) => {
           console.log(err.response)
           this.boxOne = ''
-          this.$bvModal.msgBoxOk('Invalid Email or Already In Use')
           this.error = err.response.data.error
-        })
+          this.$bvModal.msgBoxOk(this.error)
+        }
+      )
     }
   }
 }
